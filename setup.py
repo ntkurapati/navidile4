327a7eb328f7c92c73a75d266fc0f9844488f603
ntkurapati <ntkurapati@users.noreply.github.com>
2015-02-01 12:52:32 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='navidile4',
+    version='4.7',
+    packages=[''],
+    url='https://github.com/ntkurapati/navidile4',
+    license='GNU',
+    author='ntk',
+    author_email='',
+    description=''
+)
